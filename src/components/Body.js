5b7e902e224307277590b766d4ae59579090a769
Pujari Balaji <pujaribalaji0801@gmail.com>
2025-08-26 17:10:07 +0530

    Improved search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,11 +6,15 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   //State variable
   const [resList, setresData] = useState([]);
+  // This state variable is for search functionality feature
+  const [filteredrestaurants, setFilteredRestaurants] = useState([]);
+  // While updataing the res data we will this method setFilteredRestaurants
   // Here we are using useEffect Hook
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
+    // Here data will be in form of promise,because this happens with fetch()
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
@@ -18,53 +22,66 @@ const Body = () => {
     console.log(jsondata);
     setresData(
       //Here I am going to perform optinal chaining
-      jsondata.data.cards[3].card.card.gridElements.infoWithStyle.restaurants
+      jsondata.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    // Here we are using this method to call all the res data without modifying
+    // the original data. We are making a copy of it.
+    setFilteredRestaurants(
+      jsondata.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
   };
 
   // This is for search option
   const [searchText, setsearchText] = useState("");
-  const handleSearch = () => {
-    const updatedList = resList.filter((res) =>
-      res.info.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setresData(updatedList);
-  };
+  console.log("Body Rendered");
 
   // Here loading spinner we are using to load a spinner while API
   // is getting data
-  if (resList.length === 0) {
-    return <Shimmer />;
-  }
-  return (
+  // This is also called Conditional Rendering
+  // Here we are using ternary operator
+  return resList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search-container">
+          <input
+            placeholder="Search"
+            type="text"
+            value={searchText}
+            onChange={(e) => setsearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              //Filter the restaurant cards and udpate the UI
+              //searchText
+              const searchList = resList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurants(searchList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             // Here we have to write filter code bcoz we are going to show
             // only top rated restaurants
             const filteredList = resList.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating >= 4
             );
             setresData(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search"
-            value={searchText}
-            onChange={(e) => setsearchText(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-        </div>
       </div>
       {/* Here Resto-card many will be there so for that we will build seperate component */}
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {filteredrestaurants.map((restaurant) => (
           <RestaurantCard resData={restaurant} key={restaurant.info.id} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import logo from "../../assets/logo.png";
 
 // First Header component we are designing
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setLoggedIn((prev) => !prev);
+  };
   return (
     <div className="header">
       <div className="logo">
@@ -13,6 +18,11 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button className="login-button" onClick={handleLogin}>
+              {loggedIn ? "Login" : "Logout"}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
